5e9a6323362665630b55b55bc4ab3474b3a3c0c9
eshr4q <feshr4q@gmail.com>
2023-10-15 19:06:48 +0330

    5

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 var randomNumber1 = Math.random();
 randomNumber1 = randomNumber1 *6;
-randomNumber1 = Math.floor(randomNumber1);
+randomNumber1 = Math.floor(randomNumber1) + 1;
 console.log(randomNumber1);
+
+var randomDiceImage = "images/dice" + randomNumber1 + ".png";
+
+
+var image1 = document.querySelector("img");
+image1.setAttribute("src", randomDiceImage);
+
+
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomDiceImage);
+
+
+
+
+
+
+
