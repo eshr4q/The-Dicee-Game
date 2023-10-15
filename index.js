6a6d42d9efde69e8947f3ee1732734e7016ceeac
eshr4q <feshr4q@gmail.com>
2023-10-15 19:32:15 +0330

    khafantar az Angela (Milad)

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-var randomNumber1 = Math.random();
-randomNumber1 = randomNumber1 *6;
-randomNumber1 = Math.floor(randomNumber1) + 1;
-console.log(randomNumber1);
+function imageNameGenerator() {
 
-var randomDiceImage = "images/dice" + randomNumber1 + ".png";
-
+  var randomNumber1 = Math.random();
+  randomNumber1 = randomNumber1 *6;
+  randomNumber1 = Math.floor(randomNumber1) + 1;
+  var randomDiceImage = "images/dice" + randomNumber1 + ".png";
+  return randomDiceImage;
+}
+// console.log(imageNameGenerator())
 
 var image1 = document.querySelector("img");
-image1.setAttribute("src", randomDiceImage);
+image1.setAttribute("src", imageNameGenerator());
 
 
 var image2 = document.querySelectorAll("img")[1];
-image2.setAttribute("src", randomDiceImage);
+image2.setAttribute("src", imageNameGenerator());
 
 
 
